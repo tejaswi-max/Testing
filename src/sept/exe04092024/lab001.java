77c7505415412395ad8f7f58fe6df1eb0dd6e8c3
tejaswi <tejaswi.430@gmail.com>
2024-09-08 04:49:45 +0530

    add files

--- src/sept/exe04092024/lab001.java
+++ src/sept/exe04092024/lab001.java
@@ -3,5 +3,8 @@ package sept.exe04092024;
 public class lab001 {
     public static void main(String[] args) {
         System.out.println("hello")  ;
-    System.out.println(2+2);}
+    System.out.println(2+2);
+        System.out.println("Tejaswi");
+    }
+
 }
